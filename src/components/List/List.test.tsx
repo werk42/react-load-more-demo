dad1fe59148d33a631fd0697e050cccc65d0a579
Bojan Nikolov <nikolovbojan@gmail.com>
2020-09-25 17:47:36 +0200

    Updated axios mock

--- src/components/List/List.test.tsx
+++ src/components/List/List.test.tsx
@@ -1,36 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
 import AppListItem from './ListItem';
+import List from './List';
 import axios from '../../___mocks___/axios';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-// test('mocking axios request', async ()=> {
+test('mocking axios request', ()=> {
 
-//     const data = {
-//         data: [
-//             {
-//                 name: 'name 1',
-//                 model: 'model 1',
-//             },
-//             {
-//                 name: 'name 2',
-//                 model: 'model 2',
-//             },
-//             {
-//                 name: 'name 3',
-//                 model: 'model 3',
-//             }
+    const data = {
+        data: [
+            {
+                name: 'name 1',
+                model: 'model 1',
+            },
+        ]
+    }
 
-//         ]
-//     }
+    const response = {data: data}
 
-//     mockedAxios.get.mockResolvedValueOnce(data);
-//     const { getByText } = render(<><AppListItem key={name} name={name} model={model}/></>);
-//     await wait(()=> {
-//         expect(getByText('mocked name'))
-//     })
+    mockedAxios.get.mockResolvedValueOnce(response);
 
-// });
+});
 
